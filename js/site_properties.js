fcbde6c121cb3b08444847eb2b6b63708bedc7a4
Kisalay <kisalaykisu@gmail.com>
2022-08-21 12:56:09 +0530

    file added:

--- js/site_properties.js
+++
@@ -1,6 +0,0 @@
-let site_properties = {
-  use_local_storage: "false",
-  home_page: "../pages/homePage.html",
-  add_emp_payroll_page: "../pages/EmployeePayrollApp.html",
-  server_url:"http://127.0.0.1:3000/EmployeePayrollDB"
-};
